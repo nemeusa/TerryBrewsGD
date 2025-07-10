1222a7a41aeab87e195605ab26754abc87420652
Gharly <fedegvivas@gmail.com>
2025-07-09 21:48:13 -0300

    Tienda.Cs
    Tienda.Cs

--- Assets/Scripts/Tienda.cs
+++ Assets/Scripts/Tienda.cs
@@ -8,10 +8,15 @@ public class Tienda : MonoBehaviour
     [SerializeField] int _medicine;
     [SerializeField] TMP_Text _bulletText, _medicineText;
 
+    public GameObject objetoA;
+    private Color _originalColor;
+    [SerializeField] ParticleSystem _errorParticles;
     private void Start()
     {
         _bulletText.text = "$" + _bulletPrice;
         _medicineText.text = "$" + _medicinePrice;
+        _originalColor = objetoA.GetComponent<MeshRenderer>().material.color;
+
     }
     public void BuyBullet()
     {
@@ -21,8 +26,11 @@ public class Tienda : MonoBehaviour
                 _player.ReloadOneBullet();
                 _player._score -= _bulletPrice;
             }
+            else
+            {
+                TriggerErrorFeedback();
+            }
     }
-
     public void BuyMedicine()
     {
         if(_player._cordura < 100)
@@ -31,5 +39,25 @@ public class Tienda : MonoBehaviour
                 _player._cordura += _medicine;
                 _player._score -= _medicinePrice;
             }
+            else
+            {
+                TriggerErrorFeedback();
+            }
+    }
+    private void TriggerErrorFeedback()
+    {
+        objetoA.GetComponent<MeshRenderer>().material.color = Color.red;
+
+        if (_errorParticles != null)
+        {
+            _errorParticles.Play();
+        }
+
+        Invoke(nameof(ResetColor), 0.5f);
+    }
+
+    private void ResetColor()
+    {
+        objetoA.GetComponent<MeshRenderer>().material.color = _originalColor;
     }
 }
