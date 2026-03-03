7121c60dbcd3a831d4ecf3ab82a432f6af8d9b55
LucasMozzillo <lumozzillo@gmail.com>
2026-03-03 15:58:08 -0300

    agregracionessss

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Collections;
+using System.Collections.Generic;
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] Animator transitionAnim;
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+       // SceneManager.LoadScene(sceneName);
+        StartCoroutine(ChangeToSceneB(sceneName));
     }
     public void Exit()
     {
         Application.Quit();
     }
+
+    IEnumerator ChangeToSceneB(string scene)
+    {
+        transitionAnim.SetTrigger("exit");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(scene);
+    }
+
+    IEnumerator ChangeToSceneA()
+    {
+        transitionAnim.SetTrigger("exit");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(0);
+    }
+
 }
